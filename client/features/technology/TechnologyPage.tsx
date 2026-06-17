51062d9961694d3e946d4cfd7785f6d9edb219a0
vijay <vijay123kumawat123@gmail.com>
2026-06-17 12:31:26 +0530

    remove angle line

--- client/features/technology/TechnologyPage.tsx
+++ client/features/technology/TechnologyPage.tsx
@@ -4,14 +4,12 @@ import {
   WildfireDetectionShowcase,
 } from "@/features/technology/components/TechnologySections";
 import { TechnologyHero } from "@/features/technology/components/TechnologyHero";
-import { TechnologySectionTransition } from "@/features/technology/components/TechnologySectionTransition";
 import { HowItWorks } from "@/features/technology/components/how-it-works/HowItWorks";
 
 export default function TechnologyPage() {
   return (
     <PageLayout>
       <TechnologyHero />
-      <TechnologySectionTransition />
       <HowItWorks />
       <MultimodalFusion />
       <WildfireDetectionShowcase />
