46d156f5c6022f23a090cbd92a1b346b5979f0cf
vijay <vijay123kumawat123@gmail.com>
2026-08-17 22:37:21 +0530

    cleanup

--- client/App.tsx
+++ client/App.tsx
@@ -1,28 +1,28 @@
 import "./global.css";
 
 import { createRoot } from "react-dom/client";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Technology from "./pages/Technology";
-import People from "./pages/People";
-import Blog from "./pages/Blog";
-import NotFound from "./pages/NotFound";
-
-
-
-
 import ScrollToTop from "./components/common/ScrollToTop";
+
+const Home = lazy(() => import("./pages/Home"));
+const Technology = lazy(() => import("./pages/Technology"));
+const People = lazy(() => import("./pages/People"));
+const Blog = lazy(() => import("./pages/Blog"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
 const App = () => (
   <BrowserRouter>
     <ScrollToTop />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/technology" element={<Technology />} />
-      <Route path="/people" element={<People />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/technology" element={<Technology />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   </BrowserRouter>
 );
 
